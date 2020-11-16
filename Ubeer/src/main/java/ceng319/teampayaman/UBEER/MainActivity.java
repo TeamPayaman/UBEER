288b5d63478b499354c7fea3a65220321924e5ef
Arkloyd Anenias <arkloyd-android>
2020-11-15 19:27:30 -0800

    Modified some error

--- Ubeer/src/main/java/ceng319/teampayaman/UBEER/MainActivity.java
+++ Ubeer/src/main/java/ceng319/teampayaman/UBEER/MainActivity.java
@@ -7,15 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import ceng319.teampayaman.UBEER.R;
-
-
-<<<<<<< HEAD
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-=======
-public class MainActivity extends AppCompatActivity  {
->>>>>>> c2e49778c206cede200e6b845f36521d1d471d12
+
     private Button button;
     View view;
 
@@ -24,7 +17,6 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
         Button button1 = findViewById(R.id.button);
         Button button2 = findViewById(R.id.button2);
 
@@ -41,10 +33,7 @@ public class MainActivity extends AppCompatActivity  {
         }
     }
 }
-=======
-    }
-
-}
 
 
->>>>>>> c2e49778c206cede200e6b845f36521d1d471d12
+
+
