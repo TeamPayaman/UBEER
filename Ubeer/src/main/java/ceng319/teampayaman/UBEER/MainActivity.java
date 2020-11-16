f0961ebed2848e0e5bfd420f55d634656d204be7
Arkloyd Anenias <arkloyd-android>
2020-11-15 16:28:41 -0800

    First Commit

---
+++ Ubeer/src/main/java/ceng319/teampayaman/UBEER/MainActivity.java
@@ -0,0 +1,26 @@
+package ceng319.teampayaman.UBEER;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import ceng319.teampayaman.UBEER.R;
+
+
+public class MainActivity extends AppCompatActivity  {
+    private Button button;
+    View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
+
+
