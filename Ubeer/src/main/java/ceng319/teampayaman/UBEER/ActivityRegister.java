20a8c67df583aa1dd14907d43997a88296485736
josefrenzranopa <n01312587@humbermail.ca>
2020-11-15 19:39:25 -0500

    Added Activity Register

---
+++ Ubeer/src/main/java/ceng319/teampayaman/UBEER/ActivityRegister.java
@@ -0,0 +1,14 @@
+package ceng319.teampayaman.UBEER;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
