11486cf68a2e52866174060bbe4204997997cca8
josefrenzranopa <n01312587@humbermail.ca>
2020-11-15 22:04:42 -0500

    Updated ActivityLogin.java

--- Ubeer/src/main/java/ceng319/teampayaman/UBEER/ActivityLogin.java
+++ Ubeer/src/main/java/ceng319/teampayaman/UBEER/ActivityLogin.java
@@ -2,13 +2,24 @@ package ceng319.teampayaman.UBEER;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ActivityLogin extends AppCompatActivity {
+public class ActivityLogin extends AppCompatActivity implements View.OnClickListener {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button = findViewById(R.id.loginBtn);
+        button.setOnClickListener(this);
+    }
+    public void onClick(View view) {
+        Intent i = new Intent(ActivityLogin.this,ubeermain.class);
+        startActivity(i);
     }
 }
