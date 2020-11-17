12e8ad22b1ccc35a20a7c75b8d6e513a378c9d2e
josefrenzranopa <n01312587@humbermail.ca>
2020-11-17 18:07:00 -0500

    removed ubeermain.java code

--- Ubeer/src/main/java/ceng319/teampayaman/UBEER/ActivityLogin.java
+++ Ubeer/src/main/java/ceng319/teampayaman/UBEER/ActivityLogin.java
@@ -2,10 +2,12 @@ package ceng319.teampayaman.UBEER;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 
 public class ActivityLogin extends AppCompatActivity implements View.OnClickListener {
     Button button;
@@ -15,7 +17,7 @@ public class ActivityLogin extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button button = findViewById(R.id.loginBtn);
+        Button button = findViewById(R.id.loginbtn);
         button.setOnClickListener(this);
     }
     public void onClick(View view) {
--- Ubeer/src/main/java/ceng319/teampayaman/UBEER/ubeermain.java
+++ Ubeer/src/main/java/ceng319/teampayaman/UBEER/ubeermain.java
@@ -22,53 +22,5 @@ public class ubeermain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ubeermain);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_baseline_account_circle_24);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        drawer = findViewById(R.id.drawer_layout);
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        menuItem.setChecked(true);
-                        drawer.closeDrawers();
-
-                        int id = menuItem.getItemId();
-                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-
-                        if(id == R.id.nav_home){
-                            transaction.replace(R.id.fragment_container, new HomeFragment()).commit();
-                        }
-                        if(id == R.id.nav_payment){
-                            transaction.replace(R.id.fragment_container, new HomeFragment()).commit();
-                        }
-                        if(id == R.id.nav_account){
-
-                        }
-
-                        if(id == R.id.nav_exit){
-
-                        }
-
-                        return true;
-
-                    }
-                });
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment_container, new HomeFragment()).commit();
     }
-
 }
