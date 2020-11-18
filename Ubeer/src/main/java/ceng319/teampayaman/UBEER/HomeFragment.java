b80b935ee99a43fed53bd59a6fbe54a903dd29dc
josefrenzranopa <n01312587@humbermail.ca>
2020-11-17 19:47:43 -0500

    update on home fragment 2.0

--- Ubeer/src/main/java/ceng319/teampayaman/UBEER/HomeFragment.java
+++ Ubeer/src/main/java/ceng319/teampayaman/UBEER/HomeFragment.java
@@ -13,7 +13,7 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class HomeFragment extends Fragment {
-//update on home fragrament november 17
+//update on home fragment 2.0
 
     public HomeFragment() {
         // Required empty public constructor
